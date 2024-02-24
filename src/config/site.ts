aae42d277923b415d229c27937d275255446478f
Fischer Davis <fischerdavis94@gmail.com>
2024-02-24 00:45:54 -0600

    Initial commit

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Jikan",
+  description: "Personal Time Tracking App.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/sugoi1997",
+    github: "https://github.com/DevAseel",
+    docs: "https://github.com/DevAseel",
+  },
+};
---
+++ src/server/db/schema.ts
@@ -0,0 +1,33 @@
+// Example model schema from the Drizzle docs
+// https://orm.drizzle.team/docs/sql-schema-declaration
+
+import { sql } from "drizzle-orm";
+import { index, int, sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+
+/**
+ * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
+ * database instance for multiple projects.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
+ */
+export const createTable = sqliteTableCreator(
+  (name) => `price-watcher_${name}`,
+);
+
+export const link = createTable(
+  "link",
+  {
+    id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    link: text("link", { length: 256 }),
+    name: text("name", { length: 256 }),
+    price: int("price", { mode: "number" }),
+    change: int("change", { mode: "number" }),
+    createdAt: int("created_at", { mode: "timestamp" })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updatedAt: int("updatedAt", { mode: "timestamp" }),
+  },
+  (example) => ({
+    nameIndex: index("name_idx").on(example.name),
+  }),
+);
